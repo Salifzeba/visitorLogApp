5e749df389036f632b14caf266533d73d1de1c14
Salifu Zeba <salifu.zeba@amalitech.org>
2021-01-19 10:07:31 +0000

    update

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const feedRoutes = require('./routes/feed');
 
 const app = express();
-
+const port = 8000;
+const url ='mongodb+srv://sumaila:Littleman@50@cluster0.bg5k4.mongodb.net/amalp?retryWrites=true&w=majority';
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true,})
+.then(()=>{
+    console.log('Connected');
+    app.listen(8000,()=>{
+        console.log(`listing on 8000`)
+    });
+})
+.catch(err =>{
+    console.log(err);
+});
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
@@ -17,4 +28,4 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.listen(8000);
