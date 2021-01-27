7e9e973770f9c1bda6b8ae4438cd83dd7ac161e2
Salifu Zeba <salifu.zeba@amalitech.org>
2021-01-27 11:53:38 +0000

    login changes
    update login

--- controllers/visitorLogin.js
+++ controllers/visitorLogin.js
@@ -1,1 +1,81 @@
-const visitor = require ('../models/visitorLogin');
+
+const visitorLogin = require('./model/visitorLogin');
+// Handle index actions
+exports.index = function (req, res) {
+    visitorLogin.get(function (err, visitorLogin) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "Contacts retrieved successfully",
+            data: visitorLogin
+        });
+    });
+};
+// Handle create visitorLogin actions
+exports.new = function (req, res) {
+    let visitor = new visitor();
+    visitorLogin.name = req.body.name ? req.body.name : visitorLogin.name;
+    visitorLogin.visitorphone = req.body.visitorphone;
+    visitorLogin.email = req.body.email;
+    visitorLogin.hostName = req.body.hostName;
+    visitorLogin.hostRole = req.body.hostRole;
+    visitorLogin.hostDepartment = req.body.hostDepartment;
+// save the visitorLogin and check for errors
+    visitor.save(function (err) {
+        // if (err)
+        //     res.json(err);
+res.json({
+            message: 'New visitor created!',
+            data: visitor
+        });
+    });
+};
+// Handle view contact info
+exports.view = function (req, res) {
+    Contact.findById(req.params.contact_id, function (err, contact) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'Contact details loading..',
+            data: contact
+        });
+    });
+};
+// Handle update contact info
+exports.update = function (req, res) {
+Contact.findById(req.params.contact_id, function (err, contact) {
+        if (err)
+            res.send(err);
+contact.name = req.body.name ? req.body.name : contact.name;
+        contact.gender = req.body.gender;
+        contact.email = req.body.email;
+        contact.phone = req.body.phone;
+// save the contact and check for errors
+        contact.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                message: 'Contact Info updated',
+                data: contact
+            });
+        });
+    });
+};
+// Handle delete contact
+exports.delete = function (req, res) {
+    Contact.remove({
+        _id: req.params.contact_id
+    }, function (err, contact) {
+        if (err)
+            res.send(err);
+res.json({
+            status: "success",
+            message: 'Contact deleted'
+        });
+    });
+};
